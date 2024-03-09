5f6b0954e4c6f54a72d1543f71600eb9b90d0087
CamiloTello002 <camilojtello5@gmail.com>
2024-03-09 10:50:03 -0500

    Routes are loaded dynamically

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,18 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
+import { router } from "./routes";
+
+const corsOptions = {
+  origin: "*",
+};
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.json());
-const corsOptions = {
-  origin: "*",
-};
 app.use(cors(corsOptions));
+app.use(router);
 
 app.get("/", (req, res, next) => {
   res.status(201).json({
